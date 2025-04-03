e844f30b5fb3017191a667659f9dc383dbdb9303
Mikayla <ahmedkhan@hnhtechsolutions.com>
2025-04-03 21:07:29 +0500

    filter add in hom epage

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,12 @@ import BlogSection from "./_Components/Home/Blog";
 import Brand from "./_Components/Home/Brand";
 import DailyAuction from "./_Components/Home/DailyAuction";
 import ServicesSection from "./_Components/Home/ServiceSection";
+import SortMenu from './_Components/AuctionProduct/SortMenu'
 
 export default function Home() {
   return (
     <>
+    <SortMenu />
       <HomeBanner />
       <ServicesSection/>
       {/* <div className="md:mt-14">  <SubHeading heading={"DAILY AUCTIONS"} /></div>
