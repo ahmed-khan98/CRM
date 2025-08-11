fe579cbce4c4123352fa2db7e16b74eb7022a294
Mikayla <ahmedkhan@hnhtechsolutions.com>
2025-08-11 19:16:30 +0500

    'done'

--- src/app/dashboard/dashboardcount/page.jsx
+++ src/app/dashboard/dashboardcount/page.jsx
@@ -20,12 +20,12 @@ import {
 } from "lucide-react"
 import { FaGavel } from 'react-icons/fa6'
 import { motion } from "framer-motion"
-import { useAllStoreProductQuery } from "@/app/_Service/StoreProduct/page"
 import { useWonItemsQuery } from "@/app/_Services/wonProduct/page"
 import { useAllAppointmentQuery } from "@/app/_Services/appointment/page"
 import { usePenalizedProductItemsQuery } from "@/app/_Services/PenaltyFeeProduct/page"
 import { useRouter } from "next/navigation"
 import { useGetBillBoardQuery } from "@/app/_Services/services/page"
+import { useAllStoreProductQuery } from "@/app/_Services/StoreProduct/page"
 
 
 const cardVariants = {
